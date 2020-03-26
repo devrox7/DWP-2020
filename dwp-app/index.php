ba0a7e579b819bc7dc5de27bdc05df4aedc59729
Roxana Ion <ionroxana95@yahoo.com>
2020-03-26 11:54:17 +0100

    added css n js files

--- dwp-app/index.php
+++ dwp-app/index.php
@@ -2,3 +2,24 @@
 require_once('routes.php');
 echo $_GET['url'];
 ?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>DUCK SHOP</title>
+  <meta name="description" content="DUCK SHOP">
+
+
+  <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+<h1>DUCK SHOP</h1>
+
+  <script src="scripts.js"></script>
+</body>
+</html>
