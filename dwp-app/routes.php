78ea463c518d0817524b64cd4a11eaab1af94791
Roxana Ion <ionroxana95@yahoo.com>
2020-04-26 15:57:46 +0200

    changed routes path

--- dwp-app/routes.php
+++ dwp-app/routes.php
@@ -4,23 +4,23 @@ require_once('router.php');
 Route::set('', function(){
     require_once 'Views/Home.view.php';
 });
-Route::set('/dwp-app/home', function(){
+Route::set('/DWP-2020/dwp-app/home', function(){
     require_once 'Views/Home.view.php';
 });
 
-Route::set('/dwp-app/products', function(){
+Route::set('/DWP-2020/dwp-app/products', function(){
     // echo "productsss";
     require_once 'Views/Product.view.php';
 
 });
 
-Route::set('/dwp-app/admin-panel', function(){
+Route::set('/DWP-2020/dwp-app/admin-panel', function(){
     // echo "admin paneeel";
     require_once 'Views/Admin.view.php';
 
 });
 
-Route::set('/dwp-app/contact', function(){
+Route::set('/DWP-2020/dwp-app/contact', function(){
     // echo "admin paneeel";
     require_once 'Views/Contact.view.php';
 
