64ccb0c016e20eb865b106bfc429a9fdbc1f6830
RXY7 <ion.roxana27@gmail.com>
2020-05-15 00:47:29 +0200

    test.....

--- dwp-app/Controllers/Home.controller.php
+++ dwp-app/Controllers/Home.controller.php
@@ -6,18 +6,15 @@ include_once './Models/Home.model.php';
 class HomeController extends HomeModel{
 
     protected function getSpecialOffers(){
-        return $this->getSpecialOffersDB();
-        
+        return $this->getSpecialOffersDB();   
     }
 
     protected function getLatestProducts(){
         return $this->getLatestProductsDB();
-        
     }
 
     protected function getRecommendations(){
         return $this->getRecommendationsDB();
-
     }
 
 
